b4161f34b7856e70e8a96eb5459fcdde80c30652
kiwon-seo <52943322+kiwon-seo@users.noreply.github.com>
2020-02-27 16:28:16 +0900

    Update select-from-nobel.sql

--- select-from-nobel.sql
+++ select-from-nobel.sql
@@ -1,125 +1,30 @@
 /*
-Third section of sqlzoo, SELECT from Nobel
+SQL (BY SQLZOO)
+
+CHAPTER2
+SELECT from nobel
+
+# Data 설명
+nobel이라는 table 
+column은 yr(수상연도), subject(분야), winner(수상자)
+
 */
-
---#1
-/*
-Change the query shown so that it displays Nobel prizes for 1950.
-*/
-SELECT yr, subject, winner
-FROM nobel
-WHERE yr = 1950
-
---#2
-/*
-Show who won the 1962 prize for Literature.
-*/
-SELECT winner
-FROM nobel
-WHERE yr = 1962 AND subject = 'Literature'
-
---#3
-/*
-Show the year and subject that won 'Albert Einstein' his prize.
-*/
-SELECT yr, subject
-FROM nobel
-WHERE winner = 'Albert Einstein'
-
---#4
-/*
-Give the name of the 'Peace' winners since the year 2000, including 2000.
-*/
-SELECT winner
-FROM nobel
-WHERE subject = 'Peace' AND yr >= 2000
-
---#5
-/*
-Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive
-*/
-SELECT yr, subject, winner
-FROM nobel
-WHERE (yr >=1980 AND yr <=1989) AND subject = 'Literature'
-
---#6
-/*
-Show all details of the presidential winners:
-
-Theodore Roosevelt
-Woodrow Wilson
-Jimmy Carter
-*/
-SELECT *
-FROM nobel
-WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter')
-
---#7
-/*
-Show the winners with first name John
-*/
-SELECT winner
-FROM nobel
-WHERE winner LIKE 'john%'
-
---#8
-/*
-Show the Physics winners for 1980 together with the Chemistry winners for 1984.
-*/
-SELECT *
-FROM nobel
-WHERE (subject = "Physics" AND yr = '1980') OR (subject = 'Chemistry' AND yr = 1984)
-
---#9
-/*
-Show the winners for 1980 excluding the Chemistry and Medicine
-*/
-SELECT *
-FROM nobel
-WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine')
-
---#10
-/*
-Show who won a 'Medicine' prize in an early year (before 1910, not including 1910) together with winners of a 'Literature' prize in a later year (after 2004, including 2004)
-*/
-SELECT *
-FROM nobel
-WHERE (subject  = 'Medicine' AND yr < 1910) OR (subject = 'Literature' AND yr >= 2004)
-
---#11
-/*
-Find all details of the prize won by PETER GRÜNBERG
-*/
-SELECT *
-FROM nobel
-WHERE winner LIKE 'peter gr%nberg'
-
---#12
-/*
-Find all details of the prize won by EUGENE O'NEILL
-*/
-SELECT *
-FROM nobel
-WHERE winner = 'Eugene O''Neill'
-
---#13
-/*
-Knights in order
-
-List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
-*/
-SELECT winner, yr, subject
-FROM nobel
-WHERE winner LIKE 'sir%'
-ORDER BY yr DESC, winner
-
 --#14
 /*
 The expression subject IN ('Chemistry','Physics') can be used as a value - it will be 0 or 1.
-
 Show the 1984 winners ordered by subject and winner name; but list Chemistry and Physics last.
+
+Q) subject IN // 1984년의 수상자들을 열거하는데 Chemistry 와 Physics 분야는 마지막에 배치 
+
 */
 SELECT winner, subject, subject IN ('Physics','Chemistry')
 FROM nobel
 WHERE yr=1984
 ORDER BY subject IN ('Physics','Chemistry'),subject,winner
+
+
+*/
+Point
+1. Subject 칼럼이 존재할 때 Subject IN () 을 하게되면 있으면 1, 없으면 0의 데이터가 나오게 된다.
+따라서 ORDER BY subject  IN ('Physics','Chemistry')을 하게 되면 Physics,Chemistry는 1의 데이터가 되므로
+뒤에 배열되는 것이다.
