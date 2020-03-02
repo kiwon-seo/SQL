66c4c248252625576c89982c2e55dcaa2bf5a0a0
kiwon-seo <52943322+kiwon-seo@users.noreply.github.com>
2020-03-02 17:34:37 +0900

    Update select-basics.sql

--- select-basics.sql
+++ select-basics.sql
@@ -1,64 +1,30 @@
-/*
-First section of sqlzoo, SELECT basics
-*/
---#1
-/*
-The example shows the population of 'France'. Strings should be in 'single quotes';
+Using GROUP BY and HAVING.
 
-Show the population of Germany
-*/
-SELECT population
-FROM world
-WHERE name = 'Germany'
 
---#2
-/*
-The query shows the population density population/area for each country where the area is over 5,000,000 km2.
-Show the name and per capita gdp: gdp/population for each country where the area is over 5,000,000 km2
-*/
-SELECT name, gdp/population
-FROM world
-WHERE area > 5000000
+GROUP BY 구문은 SUM ,COUNT 와 같이 GROUP 별 적용하기 위해 사용된다. 
+HAVING 구문은 display되는 group에 filtering하기위해 사용된다. 
 
---#3
-/*
-Where to find some very small, very rich countries.
-We use AND to ensure that two or more conditions hold true.
+#1. For each continent show the number of countries
 
-The example shows the countries where the population is small and the gdp is high.
-Show the name and continent where the area is less than 2000 and the gdp is more than 5000000000
-*/
-SELECT name , continent
-FROM world
-WHERE area < 2000
-AND gdp > 5000000000
+SELECT continent, count(name) FROM world
+GROUP BY continent
 
---#4
-/*
-Checking a list The word IN allows us to check if an item is in a list. The example shows the name and population for the countries 'Ireland', 'Iceland' and 'Denmark'
 
-Show the name and the population for 'Denmark', 'Finland', 'Norway', 'Sweden'
-*/
-SELECT name, population
-FROM world
-WHERE name IN ('Norway', 'Sweden', 'Finland',
-                 'Denmark')
---#5
-/*
-What are the countries beginning with G? The word LIKE permits pattern matching - % is the wildcard. The examples shows countries beginning with D
+#2. For each continent show the total population
 
-Show each country that begins with G
-*/
-SELECT name
-FROM world
-WHERE name LIKE 'G%'
+SELECT continent, SUM(population) from world
+GROUP BY continent
 
---#6
-/*
-Which countries are not too small and not too big? Show the country and the area for countries with an area between 200000 and 250000. BETWEEN allows range checking - note that it is inclusive.
+#3. WHERE and GROUP BY. The WHERE filter takes place before the aggregating function. 
+For each relevant continent show the number of countries that has a population of at least 200000000.
 
-Show the area in 1000 square km. Show area/1000 instead of area
-*/
-SELECT name, area/1000
-FROM world
-WHERE area BETWEEN 200000 AND 250000
+SELECT continent , count(name) from world
+WHERE population >200000000
+GROUP BY continent
+
+#4.GROUP BY and HAVING. The HAVING clause is tested after the GROUP BY. You can test the aggregated values with a HAVING clause. 
+Show the total population of those continents with a total population of at least half a billion.
+
+SELECT continent,sum(population) from world
+GROUP BY continent
+HAVING sum(population) > 1000000000/2
